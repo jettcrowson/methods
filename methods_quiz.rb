0aff47f18601d7ac7036e69ef0eee44b12a0d589
Jett Crowson <jettcrowson@gmail.com>
2017-10-03 10:29:17 -0600

    Implement has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,5 +1,9 @@
   
 # TODO - write has_teen?
+def has_teen?(a,b,c)
+    [a,b,c].each{|number| if number >= 13 && number <= 19 then return true end}
+    return false
+end
 
 # TODO - write not_string
 
--- testing/expanded/methods_quiz_test_expanded.rb
+++ testing/expanded/methods_quiz_test_expanded.rb
@@ -6,4 +6,12 @@ require './testing/test_setup'
 require './methods_quiz'
 
 describe 'methods_quiz' do
+	describe 'has_teen?' do
+		it '13 is a teen' do
+			has_teen?(1,2,13).must_equal(true)
+		end
+		it 'no teens present' do
+			has_teen?(1,2,3).must_equal(false)
+		end
+	end
 end
